96bb738a79d76274d97816b14eb0f583d38bebb6
“Vijai <vijai.p.rajpal@gmail.com>
2024-03-29 20:10:28 -0400

    starter site

---
+++ blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7aao)h^%e2vgc-i$ek+^8i58dfjio0t*_uojv)m!4+^xwy06om"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
